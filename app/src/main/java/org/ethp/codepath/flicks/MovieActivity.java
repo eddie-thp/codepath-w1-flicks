0956e756db7ae9dd162d25b7e2ec0fab88b46db8
Unknown <eduardo_thp@yahoo.com.br>
2016-10-12 00:09:31 -0700

    Initial implementation
    Base on codepath walkthrough video
    
    http://courses.codepath.com/course_videos/intro_to_android/youtube/MW1yVzRdq5s*3Flist=PLrT2tZ9JRrf4jWZN7nIYHYYj7R8uE1jBu&index=0?title=video+walkthrough

---
+++ app/src/main/java/org/ethp/codepath/flicks/MovieActivity.java
@@ -0,0 +1,36 @@
+package org.ethp.codepath.flicks;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+
+        String nowPlayingURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+
+        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+
+        asyncHttpClient.get(nowPlayingURL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                super.onSuccess(statusCode, headers, response);
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                super.onFailure(statusCode, headers, responseString, throwable);
+            }
+        });
+    }
+}
