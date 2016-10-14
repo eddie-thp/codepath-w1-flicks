916cc1bbc6a286698fb5da2d3075c8e173bbb477
Unknown <eduardo_thp@yahoo.com.br>
2016-10-14 01:10:13 -0700

    Swipe Refresh Layout

--- app/src/main/java/org/ethp/codepath/flicks/MovieActivity.java
+++ app/src/main/java/org/ethp/codepath/flicks/MovieActivity.java
@@ -1,9 +1,11 @@
 package org.ethp.codepath.flicks;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -25,18 +27,44 @@ public class MovieActivity extends AppCompatActivity {
 
     private List<Movie> movies;
     private MovieArrayAdapter movieAdapter;
-    ListView lvItems;
+
+    private SwipeRefreshLayout swipeContainer;
+    private ListView lvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie);
 
-        lvItems = (ListView) findViewById(R.id.lvMovies);
+        // Setup the Movies adapter and list view
         movies = new ArrayList<Movie>();
         movieAdapter = new MovieArrayAdapter(this, movies);
+        lvItems = (ListView) findViewById(R.id.lvMovies);
         lvItems.setAdapter(movieAdapter);
+        // Fetch the movies
+        fetchMovies();
 
+        // Setup the swipe container view refresh listener to fetch the movies
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchMovies();
+            }
+        });
+
+        // Configure the refreshing colors
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+    }
+
+    private void fetchMovies()
+    {
         String nowPlayingURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
 
         AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
@@ -44,6 +72,7 @@ public class MovieActivity extends AppCompatActivity {
         asyncHttpClient.get(nowPlayingURL, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                movies.clear();
                 try {
                     JSONArray nowPlayingResultsJSON = response.getJSONArray("results");
                     movies.addAll(Movie.fromJSONArray(nowPlayingResultsJSON));
@@ -52,12 +81,17 @@ public class MovieActivity extends AppCompatActivity {
                     // TODO error logging
                     jsonEx.printStackTrace();
                 }
+                swipeContainer.setRefreshing(false);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 super.onFailure(statusCode, headers, responseString, throwable);
+                Toast.makeText(getBaseContext(), R.string.error_fetch_movies, Toast.LENGTH_LONG);
+                Log.e("FETCH_MOVIES", "Failed to fetch movies: " + throwable.getMessage(), throwable);
+                swipeContainer.setRefreshing(false);
             }
         });
+
     }
 }
