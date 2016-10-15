ccd0903a4bdbfecb0e2291e6c80e3fc81f76c254
Unknown <eduardo_thp@yahoo.com.br>
2016-10-15 00:40:48 -0700

    OkHTTP
    Replace AsynHTTP with OkHTTP

--- app/src/main/java/org/ethp/codepath/flicks/MovieActivity.java
+++ app/src/main/java/org/ethp/codepath/flicks/MovieActivity.java
@@ -5,10 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
-
-import com.loopj.android.http.AsyncHttpClient;
-import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.ethp.codepath.flicks.adapters.MovieArrayAdapter;
 import org.ethp.codepath.flicks.models.Movie;
@@ -16,12 +14,18 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import cz.msebera.android.httpclient.Header;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import static android.R.transition.move;
 
@@ -69,33 +73,52 @@ public class MovieActivity extends AppCompatActivity {
 
     private void fetchMovies()
     {
-        String nowPlayingURL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+        HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.themoviedb.org/3/movie/now_playing").newBuilder();
+        urlBuilder.addQueryParameter("api_key", "a07e22bc18f5cb106bfe4cc1f83ad8ed");
+        String url = urlBuilder.build().toString();
 
-        AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
+        Request request = (new Request.Builder())
+                .url(url)
+                .build();
 
-        asyncHttpClient.get(nowPlayingURL, new JsonHttpResponseHandler() {
+        OkHttpClient client = new OkHttpClient();
+
+        client.newCall(request).enqueue(new Callback() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+            public void onResponse(Call call, final Response response) throws IOException {
                 movies.clear();
                 try {
-                    JSONArray nowPlayingResultsJSON = response.getJSONArray("results");
+                    String responseData = response.body().string();
+                    JSONObject json = new JSONObject(responseData);
+                    JSONArray nowPlayingResultsJSON = json.getJSONArray("results");
                     movies.addAll(Movie.fromJSONArray(nowPlayingResultsJSON));
-                    movieAdapter.notifyDataSetChanged();
-                } catch (JSONException jsonEx) {
-                    // TODO error logging
-                    jsonEx.printStackTrace();
+                } catch (JSONException e) {
+                    Log.e("FETCH_MOVIES", "Failed to process response " + e.getMessage(), e);
                 }
-                swipeContainer.setRefreshing(false);
+
+                // Run view-related code back on the main thread
+                MovieActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        movieAdapter.notifyDataSetChanged();
+                        swipeContainer.setRefreshing(false);
+                    }
+                });
             }
 
             @Override
-            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                super.onFailure(statusCode, headers, responseString, throwable);
-                Toast.makeText(getBaseContext(), R.string.error_fetch_movies, Toast.LENGTH_LONG).show();
-                Log.e("FETCH_MOVIES", "Failed to fetch movies: " + throwable.getMessage(), throwable);
-                swipeContainer.setRefreshing(false);
+            public void onFailure(Call call, IOException e) {
+                Log.e("FETCH_MOVIES", "Failed to fetch movies: " + e.getMessage(), e);
+
+                // Run view-related code back on the main thread
+                MovieActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getBaseContext(), R.string.error_fetch_movies, Toast.LENGTH_LONG).show();
+                        swipeContainer.setRefreshing(false);
+                    }
+                });
             }
         });
-
     }
 }
