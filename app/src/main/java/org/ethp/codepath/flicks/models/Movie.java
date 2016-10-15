fe067232b53b0c53b196d3899655478fade2e6df
Unknown <eduardo_thp@yahoo.com.br>
2016-10-15 12:17:32 -0700

    Heterogeneous List Views
    Create layout for displaying popular movies

--- app/src/main/java/org/ethp/codepath/flicks/models/Movie.java
+++ app/src/main/java/org/ethp/codepath/flicks/models/Movie.java
@@ -18,20 +18,14 @@ public class Movie {
     private String overview;
     private String posterPath;
     private String backdropPath;
+    private double popularity;
 
     public Movie(JSONObject movieJson) throws JSONException {
         this.originalTitle = movieJson.getString("original_title");
         this.overview = movieJson.getString("overview");
         this.posterPath = movieJson.getString("poster_path");
         this.backdropPath = movieJson.getString("backdrop_path");
-    }
-
-    public String getPosterPath() {
-        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
-    }
-
-    public String getBackdropPath() {
-        return String.format("https://image.tmdb.org/t/p/w780/%s", backdropPath);
+        this.popularity = movieJson.getDouble("popularity");
     }
 
     public String getOriginalTitle() {
@@ -40,6 +34,20 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public boolean isPopular() {
+        // Using 10 just to see more than 1 popular item in the list
+        // I wasn't getting many popular movies from the API
+        return (popularity >= 10);
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getBackdropPath() {
+        return String.format("https://image.tmdb.org/t/p/w780/%s", backdropPath);
     }
 
     public static List<Movie> fromJSONArray(JSONArray moviesJson) {
